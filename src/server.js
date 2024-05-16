d06d75cb61499112ce7fe60f518dd87227f4a1df
Renanmdr <renanmdr3@gmail.com>
2024-05-16 16:51:36 -0300

    cabeçalhos

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 import http from 'http'
-
+const users = []
 const server =  http.createServer((req, res) => {
     const { method, url} = req
 
     if(method === 'GET' && url === '/users'){
-        return res.end('Listagem de usuarios')
+        return res.setHeader('Content-type', 'application/json').end(JSON.stringify(users))
     }
 
     if(method === 'POST' && url === '/users'){
+        users.push({
+            id: 1,
+            name: 'Joe Jr',
+            email: 'joe@gmail.com'
+        })
         return res.end('Criação de usuario')
     }
 return res.end('Hello world 2')
